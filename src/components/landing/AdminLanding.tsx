884efd15b5cb20c1dec3ed2de6106be38acd1508
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2026-05-08 10:40:06 -0700

    MAT-9641 user table beautification

--- src/components/landing/AdminLanding.tsx
+++ src/components/landing/AdminLanding.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Tabs, Tab } from "@madie/madie-design-system/dist/react";
-import { useDocumentTitle, useUserRoles } from "@madie/madie-util";
+import {
+  useDocumentTitle,
+  useUserRoles,
+  // @ts-ignore
+  useFeatureFlags,
+} from "@madie/madie-util";
 import UserManagement from "./userManagement/UserManagement";
 import "./AdminLanding.scss";
 
 const AdminLanding = () => {
   useDocumentTitle("MADiE Admin");
   const userRoles = useUserRoles();
+  const featureFlags = useFeatureFlags();
 
   if (!userRoles?.isAdmin) {
     return null;
@@ -16,17 +22,19 @@ const AdminLanding = () => {
     <div data-testid="admin-landing">
       <div id="admin-nav" style={{ marginTop: "-48px", marginLeft: "32px" }}>
         <Tabs value="user-management" type="A" size="standard">
-          <Tab
-            type="A"
-            size="standard"
-            value="user-management"
-            label="User Management"
-            data-testid="user-management-tab"
-          />
+          {featureFlags?.AdminUserList && (
+            <Tab
+              type="A"
+              size="standard"
+              value="user-management"
+              label="User Management"
+              data-testid="user-management-tab"
+            />
+          )}
         </Tabs>
       </div>
 
-      <UserManagement />
+      {featureFlags?.AdminUserList && <UserManagement />}
     </div>
   );
 };
