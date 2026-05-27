1ef55a4e8cfc19a6ff16b68d2a0b44b39ce68fd9
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-05-27 16:32:07 -0400

    MAT-10073 adding codesytemmanagement and valuesetmanagement tabs

---
+++ src/components/landing/codeSystemManagement/CodeSystemManagement.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CodeSystemManagement() {
+  return <div data-testid="code-system-management"></div>;
+}
---
+++ src/components/landing/valueSetManagement/ValueSetManagement.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ValueSetManagement() {
+  return <div data-testid="value-set-management"></div>;
+}
